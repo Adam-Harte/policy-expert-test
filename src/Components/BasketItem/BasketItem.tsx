a3e107f74b06bcfce8b5ce85939e7cbe6079bca4
Adam-Harte <adamharte11@hotmail.co.uk>
2020-12-08 23:54:39 +0000

    logic tweak for disabling decrease button

--- src/Components/BasketItem/BasketItem.tsx
+++ src/Components/BasketItem/BasketItem.tsx
@@ -27,7 +27,7 @@ export const BasketItem: React.FC<BasketItemProps> = ({ item }) => {
       <img src={item.img} alt={item.name} />
       <button
         type="button"
-        disabled={item.quantity <= 1 || item.litres <= 0.175}
+        disabled={item.quantity <= 1 && item.litres <= 0.175}
         onClick={() => decrease(item)}> - </button>
       <span>{item.quantity}</span>
       <button type="button" onClick={() => increase(item)}> + </button>
